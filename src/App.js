14765b19bca850d8817df02bb6d30197c176e3e6
manuelpz <manuel.lopez.fernandez97@gmail.com>
2022-10-16 17:26:36 +0200

    Creación del sitio web

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          La página web de Lady Verónica.
         </p>
         <a
           className="App-link"
