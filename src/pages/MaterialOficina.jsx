14e44ef0d077a5632b943c7f38c2df8a12069613
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2023-05-17 19:29:42 +0200

    Añadido tailwind, arreglo del input de material de oficina y estilos

--- src/pages/MaterialOficina.jsx
+++ src/pages/MaterialOficina.jsx
@@ -9,28 +9,31 @@ export default function MaterialOficina() {
     const [value, setValue] = useState({ input: "" })
     const [cardList, setCardList] = useState ([])
     const [hayProducto, setHayProducto] = useState(false)
-
-
-    useEffect(() => {
+    const checkInput = () => {
         if (value.input !== "") {
             const newList = cards.filter(card => card.hasOwnProperty("elementos") && (card.elementos.toLowerCase().includes(value.input) || card.elementos.includes(value.input)))
             setCardList(newList);
             setHayProducto(true)
         }
-        else if (value.input === ""){
+        else if (value.input === "") {
             const newList = cards
             setCardList(newList);
             setHayProducto(false)
-        } 
+        }
         else {
             setCardList([])
             setHayProducto(false)
         }
+    }
+
+    useEffect(() => {
+        checkInput()
     }, [value.input]);
 
     const limpia = () =>{
         setValue({ input: "" })
         setCardList([])
+        checkInput()
     }
 
 
@@ -40,24 +43,24 @@ export default function MaterialOficina() {
             <p>Disponemos de más de 3.000 productos de material de oficina para tu empresa. Papelería, material escolar, papelería corporativa, consumibles informáticos y máquinas.Ofrecemos productos de papelería de marcas de gran prestigio.Puedes consultarnos los productos que tenemos disponibles.</p>
             <label className="input"><b><i>¿Buscas algo en concreto?</i></b></label>
             <br />
-            <div className="input">
-                <input type="text" value={value.input} onChange={e =>{
+            <div className="input ">
+                <input type="text" id="first_name" className="bg-white border-2 border-black text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 mr-2 p-2.5 dark:bg-gray-700 dark:border-gray dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 lg:h-1/5 lg:w-1/5 h-3/5 w-3/5" placeholder="Grapas, gomas, sacapuntas..." value={value.input} onChange={e =>{
                     setValue({ input: e.target.value })
                    }} />
-                <button onClick={limpia} >X</button>
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" onClick={limpia} >X</button>
             </div>
             <br />
             <Container>
                 {cardList.length > 0 ? (
                     <>
-                        {hayProducto ? <h1>¡Disponemos del producto <b>{value.input}</b> ✔!</h1> :"" }   
+                        {hayProducto ? <h1>¡Disponemos del producto <b>{value.input}</b> ✅!</h1> :"" }   
                 <ul>
                     {cardList.map(card => <CartaOficina key={card.id} card={card} />)}
                     
                 </ul>
                     </>
                     ): (
-                    <h1>Lo sentimos, no disponemos de "{value.input}" </h1>
+                        <h1>Lo sentimos, no disponemos de "{value.input}" ❌ </h1>
                 )}
             </Container>
         </>
