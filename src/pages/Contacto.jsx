5a2b5f185f1fabaf4de243730a3d967cce933460
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2023-05-18 23:07:18 +0200

    Primera version para produccion de SOMOS A3 VERSION 1.0.0 - CERRADA

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import emailjs from "emailjs-com";
-import { Form, Input, TextArea, Button } from "semantic-ui-react";
+import { Form, Input, TextArea } from "semantic-ui-react";
 import Swal from "sweetalert2";
 
 import "../styles/contacto.css";
@@ -78,9 +78,19 @@ const Contacto = () => {
                 placeholder="Mensaje..."
                 required
               />
-              <Button type="submit" color="green">
-                Enviar
-              </Button>
+              <div className="grid justify-center">
+                <button className="botonContacto" type="submit">
+                  <div className="svg-wrapper-1">
+                    <div className="svg-wrapper">
+                      <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z" fill="currentColor"></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <span>Enviar</span>
+                </button>
+              </div>
             </Form>
           </div>
           <div>
--- src/pages/Higiene.jsx
+++ src/pages/Higiene.jsx
@@ -18,9 +18,15 @@ export default function Higiene() {
     return (
         <div>
             <Container>
+                <h2 className="disponibilidadProductos">Disponemos de múltiples productos de limpieza para su empresa o su hogar, para más información consultemos.</h2>
                 <Tarjeta cards={cards}></Tarjeta>
-            <h1 className="disponibilidadProductos">Disponemos de múltiples productos de limpieza para su empresa o su hogar, para más información consultemos.</h1>
-            <Link to={"/contacto"}><button className="btn btn-outline-dark">¡Quiero contactar!</button></Link>
+                <Link className="flex justify-center items-center !no-underline" to={"/contacto"}>
+                    <button className="cssbuttons-io-button "> ¡Contactar!
+                            <div className="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+                            </div>
+                        </button>
+                </Link>
             </Container>
 
         </div>
