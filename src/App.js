7fee2bf928f03a9f13b8cb6e795d079d08c837dd
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2023-06-29 00:35:36 +0200

    Hotfix rutas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Footer from "./components/Footer"
 import Navegationbar from "./components/Navbar"
 import Inicio from "./pages/Inicio"
