38ed3f6df7e616b45ee641a141a2e61f8716300e
Manuel Lopez Fernandez <manuel.lopez.fernandez97@gmail.com>
2023-06-29 00:45:45 +0200

    Correccion en las rutas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react"
 import "./App.css"
-import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom"
 import Footer from "./components/Footer"
 import Navegationbar from "./components/Navbar"
 import Inicio from "./pages/Inicio"
@@ -23,20 +28,22 @@ function App() {
     <div className="App">
       <Router>
         <Navegationbar />
-        <Routes>
-          <Route exact path="/" element={<Inicio />} />
-          <Route exact path="/plan" element={<Plan />} />
-          <Route exact path="/nosotros" element={<Nosotros />} />
-          <Route exact path="/contacto" element={<Contacto />} />
-          <Route exact path="/caramelos" element={<Caramelos />} />
-          <Route exact path="/oficina" element={<MaterialOficina />} />
-          <Route exact path="/celulosa" element={<Celulosa />} />
-          <Route exact path="/guantes" element={<Guantes />} />
-          <Route exact path="/bolsas" element={<Bolsas />} />
-          <Route exact path="/higiene" element={<Higiene />} />
-          <Route exact path="/alimentacion" element={<Alimentacion />} />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
+        <Switch>
+          <Routes>
+            <Route exact path="/" element={<Inicio />} />
+            <Route exact path="/plan" element={<Plan />} />
+            <Route exact path="/nosotros" element={<Nosotros />} />
+            <Route exact path="/contacto" element={<Contacto />} />
+            <Route exact path="/caramelos" element={<Caramelos />} />
+            <Route exact path="/oficina" element={<MaterialOficina />} />
+            <Route exact path="/celulosa" element={<Celulosa />} />
+            <Route exact path="/guantes" element={<Guantes />} />
+            <Route exact path="/bolsas" element={<Bolsas />} />
+            <Route exact path="/higiene" element={<Higiene />} />
+            <Route exact path="/alimentacion" element={<Alimentacion />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </Switch>
       </Router>
       <Footer />
     </div>
